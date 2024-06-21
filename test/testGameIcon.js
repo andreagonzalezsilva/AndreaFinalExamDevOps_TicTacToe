15cc411bc0b4ee5f3f2a351f93f03322011dc371
andreagonzalezsilva <andreagonzalezs1708@gmail.com>
2024-06-20 20:47:05 -0700

    Update the test file version

--- test/testGameIcon.js
+++ test/testGameIcon.js
@@ -1,9 +1,7 @@
-const { By, Builder } = require('selenium-webdriver');
+const {By, Builder, Browser} = require('selenium-webdriver');
 const chrome = require("selenium-webdriver/chrome");
 const assert = require("assert");
-
-const expectedFirstPlayerIcon = "×";
-const errorIcon = "playerText";
+const expectedFirstPlayerIcon = "x";
 
 (async function gameIconTest() {
 
@@ -19,38 +17,28 @@ const errorIcon = "playerText";
     driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
     await driver.get('http://54.196.103.148/');
 
-    // Maximize the window
+    // Maxime the window
     driver.manage().window().maximize();
-    await driver.manage().setTimeouts({ implicit: 500 });
+    await driver.manage().setTimeouts({implicit: 500});
 
     // Create the first window element
     let gamePreferencesWindow = await driver.findElement(By.className('modal-content'));
     let playBtn = await driver.findElement(By.id('okBtn'));
 
     // Click on the play button 
-    await playBtn.click();
+    playBtn.click();
 
     // Start the game putting the expected X in the superior left corner
     let gameCell = await driver.findElement(By.id('cell0'));
-    await gameCell.click();
+    gameCell.click();
     let firstPlayerIcon = await gameCell.getText();
 
-    // Log the actual icon text
-    console.log('First player icon:', firstPlayerIcon);
-
-    // Assert the game cell should not contain the error icon
-    if(!firstPlayerIcon.includes(errorIcon)){
-      console.log("Test Success");
-    }
-    else{
-      console.log("Test Failed")
-    }
+    // Assert the game cell should be x
+    assert.equal(firstPlayerIcon, expectedFirstPlayerIcon);
 
   } catch (e) {
-    console.error('An error occurred: ', e);
+    console.log('An error occurred: ', e);
   } finally {
-    if (driver) {
       await driver.quit();
-    }
   }
-}());
+}())
