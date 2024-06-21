2fcd75609b37795150e0bac122d9a9671dad86ac
andreagonzalezsilva <andreagonzalezs1708@gmail.com>
2024-06-20 20:38:19 -0700

    Refactored test case

--- test/testGameIcon.js
+++ test/testGameIcon.js
@@ -1,6 +1,7 @@
-const {By, Builder, Browser} = require('selenium-webdriver');
+const { By, Builder } = require('selenium-webdriver');
 const chrome = require("selenium-webdriver/chrome");
 const assert = require("assert");
+
 const expectedFirstPlayerIcon = "×";
 const errorIcon = "playerText";
 
@@ -13,34 +14,38 @@ const errorIcon = "playerText";
   options.setChromeBinaryPath('/usr/bin/google-chrome');
 
   let driver;
-  
+
   try {
     driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
     await driver.get('http://54.196.103.148/');
 
-    // Maxime the window
+    // Maximize the window
     driver.manage().window().maximize();
-    await driver.manage().setTimeouts({implicit: 500});
-  
+    await driver.manage().setTimeouts({ implicit: 500 });
+
     // Create the first window element
     let gamePreferencesWindow = await driver.findElement(By.className('modal-content'));
     let playBtn = await driver.findElement(By.id('okBtn'));
 
     // Click on the play button 
-    playBtn.click();
+    await playBtn.click();
 
     // Start the game putting the expected X in the superior left corner
     let gameCell = await driver.findElement(By.id('cell0'));
-    gameCell.click();
+    await gameCell.click();
     let firstPlayerIcon = await gameCell.getText();
 
-    // Assert the game cell should be x
-    //assert.equal(firstPlayerIcon, expectedFirstPlayerIcon);
-    assert(!firstPlayerIcon.includes(errorIcon));
-   
+    // Log the actual icon text
+    console.log('First player icon:', firstPlayerIcon);
+
+    // Assert the game cell should not contain the error icon
+    assert(!firstPlayerIcon.includes(errorIcon), `Error: The cell contains the forbidden character "${errorIcon}"`);
+
   } catch (e) {
-    console.log('An error occurred: ', e);
+    console.error('An error occurred: ', e);
   } finally {
+    if (driver) {
       await driver.quit();
+    }
   }
-}())
+}());
